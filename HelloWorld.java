55afd4f9620e14e552c3f6e2f0b30bfb9e887ac2
abdu594 <abdoahmad216200@gmail.com>
2026-09-22 20:58:31 +0200

    from VSC to INt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from IntelliJ!");
+        System.out.println("Halløj from IntelliJ!");
     }
 }
